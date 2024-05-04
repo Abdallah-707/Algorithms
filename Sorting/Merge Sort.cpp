03aa9da469c5b9cd214e4be100e3aef0c7387a2a
Abdallah Hesham Ammar <56886882+Abdallah-707@users.noreply.github.com>
2024-05-04 18:03:53 +0300

    Update Merge Sort.cpp

--- Sorting/Merge Sort.cpp
+++ Sorting/Merge Sort.cpp
@@ -8,79 +8,41 @@ using namespace std;
 -Space Complexity: O(N)
 -Time Complexity: O(N log N)
 */
-void merge(int arr[], int const left, int const mid,
-           int const right)
-{
-    int const subArr1 = mid - left + 1;
-    int const subArr2 = right - mid;
-
-    // Create temp arrays
-    auto *leftArr = new int[subArr1],
-            *rightArr = new int[subArr2];
-
-    // Copy data to temp arrays leftArr[] and rightArr[]
-    for (auto i = 0; i < subArr1; i++)
-        leftArr[i] = arr[left + i];
-    for (auto j = 0; j < subArr2; j++)
-        rightArr[j] = arr[mid + 1 + j];
-
-    auto idxOfSubArr1 = 0, idxOfSubArr2 = 0;
-    int idxOfMergedArr = left;
-
-    // Merge the temp arrays back into arr[left..right]
-    while (idxOfSubArr1 < subArr1
-           && idxOfSubArr2 < subArr2) {
-        if (leftArr[idxOfSubArr1]
-            <= rightArr[idxOfSubArr2]) {
-            arr[idxOfMergedArr]
-                    = leftArr[idxOfSubArr1];
-            idxOfSubArr1++;
+void merge(int arr[], int left, int mid, int right) {
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+    int *temp = new int[right + 1];
+    while (i <= mid || j <= right) {
+        if (i > mid) {
+            temp[k++] = arr[j++];
+        }
+        else if (j > right) {
+            temp[k++] = arr[i++];
+        }
+        else if (arr[i] > arr[j]) {
+            temp[k++] = arr[j++];
         }
         else {
-            arr[idxOfMergedArr]
-                    = rightArr[idxOfSubArr2];
-            idxOfSubArr2++;
+            temp[k++] = arr[i++];
         }
-        idxOfMergedArr++;
     }
-
-    // Copy the remaining elements of left[], if there are any
-    while (idxOfSubArr1 < subArr1) {
-        arr[idxOfMergedArr]
-                = leftArr[idxOfSubArr1];
-        idxOfSubArr1++;
-        idxOfMergedArr++;
+    for (int k = left; k <= right; k++) {
+        arr[k] = temp[k];
     }
-
-    // Copy the remaining elements of right[], if there are any
-    while (idxOfSubArr2 < subArr2) {
-        arr[idxOfMergedArr]
-                = rightArr[idxOfSubArr2];
-        idxOfSubArr2++;
-        idxOfMergedArr++;
-    }
-    delete[] leftArr;
-    delete[] rightArr;
 }
 
-// begin is for left index and end is right index
-// of the sub-array of arr to be sorted
-void mergeSort(int array[], int const begin, int const end)
-{
-    if (begin >= end)
-        return;
-
-    int mid = begin + (end - begin) / 2;
-    mergeSort(array, begin, mid);
-    mergeSort(array, mid + 1, end);
-    merge(array, begin, mid, end);
+void mergeSort(int arr[], int start, int end) {
+    if (start >= end)
+           return;
+    int mid = (start + end) / 2;
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid + 1, end);
+    merge(arr, start, mid, end);
 }
 
-
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++) {
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -89,6 +51,6 @@ void printArray(int arr[], int size)
 signed main() {
     int arr[3] = {3 ,1 ,2};
     int size = sizeof(arr)/sizeof(arr[0]);
-    mergeSort(arr,0,size-1);
-    printArray(arr,size);
+    mergeSort(arr, 0, size - 1);
+    printArray(arr, size);
 }
